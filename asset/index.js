919494736f8ee249a3108bed842ec4903bce950d
苦力怕水 <creeperwater@163.com>
2022-12-02 11:32:29 +0800

    网站剪彩完工！

--- asset/index.js
+++ asset/index.js
@@ -12,6 +12,7 @@ $('main>aside>p').forEach(ele => {
     }else{get(`https://api.github.com/repos/creeperwater/ph/contents/${this.textContent}`).then((res)=>{
       content=JSON.parse(res);
       render(content);
+      cache[this.textContent]=content;
     }).catch(()=>{
       $('main>article').innerText=`从左侧列表选择一个分类来浏览\n列表加载失败`;
     })}
@@ -25,3 +26,22 @@ function render(list){
   }
   $('main>article').innerHTML=list_con;  
 }
+
+$('header>input').oninput=function(){if(this.value){
+  var list = [];
+  for(let i in cache){
+    for(let j of cache[i]){
+      const loc = j.name.indexOf(this.value);
+      if(loc>-1){
+        j.index=loc;
+        list.push(j);
+      }
+    }
+  }
+  if(list.length){
+    list.sort((a,b)=>a.index-b.index);
+    render(list);
+  }else{
+    $('main>article').innerText= '没搜着'
+  }
+}}
